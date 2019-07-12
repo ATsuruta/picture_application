4bcc1ce4888c39e23c58a7ce364a0277e3addbf4
ATsuruta <tsuruta.0627a@gmail.com>
2019-07-12 17:01:50 +0900

    プロジェクト名をlaravelに設定

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'ja',
 
     /*
     |--------------------------------------------------------------------------
@@ -169,11 +169,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        laravel\Providers\AppServiceProvider::class,
+        laravel\Providers\AuthServiceProvider::class,
+        // laravel\Providers\BroadcastServiceProvider::class,
+        laravel\Providers\EventServiceProvider::class,
+        laravel\Providers\RouteServiceProvider::class,
 
     ],
 
